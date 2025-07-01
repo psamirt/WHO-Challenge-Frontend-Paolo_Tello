5ce21673ff02292e82abfe7f029dcc1a9e4d1cdc
psamirt <p_samir@hotmail.com>
2025-06-30 23:41:25 -0500

    animacion simple

--- src/components/CardProducts.tsx
+++ src/components/CardProducts.tsx
@@ -5,7 +5,7 @@ import { useCartStore } from "../store/cartStore";
 import { FaRegStar } from "react-icons/fa";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-// import Filters from "./Filters";
+import { AnimatePresence, motion } from "motion/react";
 
 const CardProducts: React.FC<CardProductsProps> = ({ products }) => {
   const { addToCart } = useCartStore();
@@ -31,48 +31,55 @@ const CardProducts: React.FC<CardProductsProps> = ({ products }) => {
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 card">
           {products?.map((product) => (
-            <div
-              key={product.id}
-              className="rounded-lg p-1 shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300 bg-base-200 cursor-pointer card-body  justify-between flex flex-col w-[300px] mx-auto"
-              onClick={() => handleProductClick(product)}
-            >
-              <div className="relative h-48 overflow-hidden">
-                <Image
-                  src={product.image}
-                  alt={product.title}
-                  fill
-                  className="object-contain p-4"
-                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw"
-                />
-              </div>
-              <div className="p-4 flex flex-col justify-between h-[165px]">
-                <h3 className="text-lg font-semibold mb-2 line-clamp-2 card-title">
-                  {product.title}
-                </h3>
-                <div className="flex items-center mb-2">
-                  {product.rating && (
-                    <div className="flex items-center">
-                      <FaRegStar className="w-4 h-4 text-yellow-400 fill-current" />
-                      <span className="ml-1 text-sm">
-                        {product.rating.rate} ({product.rating.count})
-                      </span>
-                    </div>
-                  )}
+            <AnimatePresence key={product.id}>
+              <motion.div
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                transition={{ duration: 0.3 }}
+                className="rounded-lg p-1 shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300 bg-base-200 cursor-pointer card-body  justify-between flex flex-col w-[300px] mx-auto"
+                onClick={() => handleProductClick(product)}
+              >
+                <div className="relative h-48 overflow-hidden">
+                  <Image
+                    src={product.image}
+                    alt={product.title}
+                    fill
+                    className="object-contain p-4"
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw"
+                  />
                 </div>
-                <div className="flex items-center justify-between">
-                  <span className="text-xl font-bold ">S/{product.price}</span>
-                  <button
-                    className="btn btn-primary"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      handleAddToCart(product);
-                    }}
-                  >
-                    Agregar
-                  </button>
+                <div className="p-4 flex flex-col justify-between h-[165px]">
+                  <h3 className="text-lg font-semibold mb-2 line-clamp-2 card-title">
+                    {product.title}
+                  </h3>
+                  <div className="flex items-center mb-2">
+                    {product.rating && (
+                      <div className="flex items-center">
+                        <FaRegStar className="w-4 h-4 text-yellow-400 fill-current" />
+                        <span className="ml-1 text-sm">
+                          {product.rating.rate} ({product.rating.count})
+                        </span>
+                      </div>
+                    )}
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <span className="text-xl font-bold ">
+                      S/{product.price}
+                    </span>
+                    <button
+                      className="btn btn-primary"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleAddToCart(product);
+                      }}
+                    >
+                      Agregar
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </div>
+              </motion.div>
+            </AnimatePresence>
           ))}
         </div>
       )}
